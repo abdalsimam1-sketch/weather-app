2e55f07d50bba597dd1bfca39a2772e8a07a3dbc
abdalsimam1-sketch <abdalsimam1@gmail.com>
2026-01-16 15:35:43 +0100

    api mounting before decoration

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import logo from "../assets/images/logo.svg";
+import { getWeatherData } from "../services/weatherAPI";
 
 export const HomePage = () => {
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [hourly, setHourly] = useState(null);
+  const [daily, setDaily] = useState(null);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const fetchWeather = async () => {
+      setLoading(true);
+      const data = await getWeatherData();
+      if (data) {
+        setCurrentWeather(data.current);
+        setHourly(data.hourly);
+        setDaily(data.daily);
+      }
+      setLoading(false);
+    };
+    fetchWeather();
+  }, []);
   return (
     <>
-      <div className="home-page text-center  ">
+      <div className="home-page text-center d-flex flex-column ">
         <nav className="d-flex justify-content-between p-5 ">
           <img src={logo} alt="logo" />
           <div className="btn btn-primary d-flex align-items-center gap-3">
@@ -28,15 +46,95 @@ export const HomePage = () => {
         </div>
 
         <div
-          className="weather-section d-flex"
+          className="weather-section d-flex flex-column  flex-md-row gap-3 align-self-center p-5 mb-5"
           style={{ border: "1px solid black" }}
         >
-          <div className="first-column">
-            <div className="row row1"></div>
-            <div className="row"></div>
-            <div className="row"></div>
+          <div className="weather-left d-flex flex-column gap-5">
+            {loading ? (
+              <div className="weather-main-card d-flex justify-content-center">
+                <h2>Loading ....</h2>
+              </div>
+            ) : (
+              <div
+                className="weather-main-card "
+                style={{ borderRadius: "1rem" }}
+              >
+                <h2>Current: {currentWeather.temperature_2m}</h2>
+              </div>
+            )}
+            <div className="stat-grid d-flex flex-column flex-md-row gap-3 justify-content-between">
+              {" "}
+              <div className="stat-card ">
+                {loading ? (
+                  <h2>Loading...</h2>
+                ) : (
+                  <div>
+                    {" "}
+                    <p>Feels like:{currentWeather.temperature_2m}</p>
+                  </div>
+                )}
+              </div>
+              <div className="stat-card ">
+                {" "}
+                {loading ? (
+                  <h2>Loading...</h2>
+                ) : (
+                  <div>
+                    {" "}
+                    <p>Humiduty:{currentWeather.relative_humidity_2m}</p>
+                  </div>
+                )}
+              </div>
+              <div className="stat-card ">
+                {" "}
+                {loading ? (
+                  <h2>Loading...</h2>
+                ) : (
+                  <div>
+                    {" "}
+                    <p>Wind:{currentWeather.wind_speed_10m}</p>
+                  </div>
+                )}
+              </div>
+              <div className="stat-card ">
+                {" "}
+                {loading ? (
+                  <h2>Loading...</h2>
+                ) : (
+                  <div>
+                    {" "}
+                    <p>Precipitation:{currentWeather.precipitation}</p>
+                  </div>
+                )}
+              </div>
+            </div>
+            {loading ? (
+              <p>Loading..</p>
+            ) : (
+              <div className="daily-forecast d-flex flex-column flex-md-row gap-5 ">
+                {daily.time.map((day, index) => (
+                  <div key={index} className="daily-card ">
+                    <p>{day}</p>
+                  </div>
+                ))}
+              </div>
+            )}
           </div>
-          <div className="second-column"></div>
+          <div className="weather-right d-flex flex-column ">
+            <div className="hourly-forecast">
+              {loading ? (
+                <p>Loading...</p>
+              ) : (
+                hourly.time.slice(3, 12).map((hour, index) => {
+                  return (
+                    <div className="hourly-card" key={index}>
+                      <p>Hour : {hour}</p>
+                    </div>
+                  );
+                })
+              )}
+            </div>
+          </div>
         </div>
       </div>
     </>
--- src/services/weatherAPI.jsx
+++ src/services/weatherAPI.jsx
@@ -1,3 +1,13 @@
 export const getWeatherData = async () => {
-  const response = await fetch();
+  const url =
+    "https://api.open-meteo.com/v1/forecast?latitude=9.0579&longitude=7.4951&daily=weather_code&hourly=temperature_2m,weather_code&current=weather_code,wind_speed_10m,temperature_2m,relative_humidity_2m,precipitation&temperature_unit=fahrenheit&timezone=auto";
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.error("Error fetching weather : ", error);
+  }
 };
